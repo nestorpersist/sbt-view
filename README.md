61bc49d35e028ac19ef65bb3e7644a19c004a588
nestor <nestor@persist.com>
2015-10-13 14:59:21 -0700

    first version

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-# sbt-view
-SBT Plugin for Viewing ScalaDoc and JavaDoc
+Opens a browser window with Javadoc/Scaladoc
+for the current project or its dependencies.
 
-Coming soon!
+
+1. In project/plugins.sbt add
+
+     addSbtPlugin("com.persist" % "sbt-view" % "0.0.4")
+
+2. In build.sbt add
+
+     viewSettings
+
+3. Make sure that your SBT dependencies include the javadoc jar.
+   Here is an example
+
+    "com.foo" %% "foopackage" % "1.2.3" withJavadoc()
+
+4. You can now use the new sbt view command
+
+        a. View Scaladoc for current project. Calls doc if none there.
+
+             view
+
+        b. Finds an item in the classpath where all words are matched.
+           Unpack the javadoc jar (if not already done) in the ivy2
+           cache and then view it.
+
+             view WORD1 WORD2 ...
+
+Note: The browser window opens automatically only on OSX.
+
+Based on Whitepages
+
+Supported by 47 Degrees.
+
+
---
+++ src/main/scala/com/persist/View.scala
@@ -0,0 +1,87 @@
+package com.persist
+
+import sbt._
+import Keys.doc
+import Keys.dependencyClasspath
+import Keys.scalaVersion
+import Keys.Classpath
+import complete.DefaultParsers._
+import java.io.File
+
+object View extends Plugin {
+
+  private case class pinfo(path: String, prefix: String, ver: String, pack: String, id: String)
+
+  private def parsePath(path: String) = {
+    val i = path.replace("-SNAPSHOT", "*SNAPSHOT").lastIndexOf("-")
+    val j = path.replace("/bundles/", "/jars/").indexOf("/jars/")
+    val prefix = path.substring(0, j)
+    val parts = prefix.split("/")
+    val pack = parts(parts.size - 2).replace(".scala", "").replace(".", "/")
+    val ver = path.substring(i + 1).replace(".jar", "")
+    val id = s"${parts(parts.size - 1)}-$ver"
+    pinfo(path, prefix, ver, pack, id)
+  }
+
+  private def open(path: String): Unit = {
+    s"open $path".!
+    println(s"opening browser window for $path")
+  }
+
+  private def showProjectDoc(sver:String, genDoc: ()=> Unit): Unit = {
+    val path = s"target/scala-$sver/api/index.html"
+    genDoc()
+    open(path)
+  }
+
+  private def viewAct(args: Seq[String], v: Classpath, sver:String, genDoc: ()=> Unit) {
+    if (args.size == 0) {
+      showProjectDoc(sver, genDoc)
+    } else {
+      val paths = v.files map (_.getPath)
+      val selected = paths filter {
+        path => args.forall(path.contains(_))
+      } filter (_.endsWith(".jar")) map (parsePath(_))
+      if (selected.size == 0) {
+        println("Nothing selected")
+      } else if (selected.size > 1) {
+        println("Too many selected")
+        for ((pp, i) <- selected.zipWithIndex) {
+          println(s"[${i + 1}] $pp")
+        }
+      } else {
+        val select = selected(0)
+        val out = s"${select.prefix}/docs/html-${select.ver}"
+        val outf = new File(out)
+        val in = s"${select.prefix}/docs/${select.id}-javadoc.jar"
+        val inf = new File(in)
+        if (!outf.exists) {
+          if (inf.exists) {
+            outf.mkdir()
+            s"tar -xf $in -C $out".!
+          } else {
+            println(s"${in} not present")
+          }
+        }
+        if (outf.exists) {
+          val index = s"${out}/index.html"
+          open(index)
+        }
+      }
+    }
+  }
+
+  val view = InputKey[Unit]("view", "view from doc jar")
+
+  val view1 = view := {
+    val args = spaceDelimited("<arg>").parsed
+    val cp = (dependencyClasspath in Runtime).value
+    def genDoc():Unit  = (doc in Compile).value
+    val sver = scalaVersion.value
+    val sver1 = sver.split("[.]").dropRight(1).mkString(".")
+    viewAct(args, cp, sver1, genDoc)
+  }
+
+  lazy val viewSettings = Seq(view1)
+}
+
